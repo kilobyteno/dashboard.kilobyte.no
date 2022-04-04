e00ae4fc27cb8e4624f10febcd93a1d1ab0104da
Daniel S. Billing <daniel@kilobyte.no>
2022-04-04 20:45:00 +0200

    Installed aglipanci/laravel-dashboard-forge-tile

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(aglipanci\ForgeTile\Commands\FetchForgeServersCommand::class)->hourly();
+        $schedule->command(aglipanci\ForgeTile\Commands\FetchForgeRecentEventsCommand::class)->everyMinute();
     }
 
     /**
--- resources/views/dashboard-blade-view.blade.php
+++ resources/views/dashboard-blade-view.blade.php
@@ -1,2 +1,4 @@
 <x-dashboard>
+    <livewire:forge-server-tile position="a2"/>
+    <livewire:forge-recent-events-tile position="a3"/>
 </x-dashboard>
