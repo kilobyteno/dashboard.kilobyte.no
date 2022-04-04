2220f993728ae922f07658ca9795000fee64cd58
Daniel S. Billing <daniel@kilobyte.no>
2022-04-04 21:24:58 +0200

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-dashboard>
     <livewire:date-time-tile position="a1:a3" timezone="Europe/Oslo" />
     <livewire:forge-server-tile position="b1:b3"/>
-    <livewire:forge-recent-events-tile position="c1:c3"/>
-    <livewire:packagist-tile position="a4:e7" />
+    <livewire:forge-recent-events-tile position="e1:e6"/>
+    <livewire:packagist-tile position="a4:d6" />
 </x-dashboard>
