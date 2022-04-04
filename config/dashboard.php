226480fa2a99af0bf07b2d7548a43792bb575b5b
Daniel S. Billing <daniel@kilobyte.no>
2022-04-04 20:45:29 +0200

    Create dashboard.php

---
+++ config/dashboard.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    /*
+     * The dashboard supports these themes:
+     *
+     * - light: always use light mode
+     * - dark: always use dark mode
+     * - device: follow the OS preference for determining light or dark mode
+     * - auto: use light mode when the sun is up, dark mode when the sun is down
+     */
+    'theme' => 'device',
+
+    /*
+     * When the dashboard uses the `auto` theme, these coordinates will be used
+     * to determine whether the sun is up or down.
+     */
+    'auto_theme_location' => [
+        'lat' => 51.260197,
+        'lng' => 4.402771,
+    ],
+
+    /*
+     * These scripts will be loaded when the dashboard is displayed.
+     */
+    'scripts' => [
+        'alpinejs' => 'https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js',
+    ],
+
+    /*
+     * These stylesheets will be loaded when the dashboard is displayed.
+     */
+    'stylesheets' => [
+        'inter' => 'https://rsms.me/inter/inter.css'
+    ],
+
+    'tiles' => [
+        'forge' => [
+            'token' => env('FORGE_API_TOKEN'),
+            'servers' => [
+                'refresh_interval_in_seconds' => 3600
+            ],
+            'recent_events' => [
+                'refresh_interval_in_seconds' => 60
+            ],
+        ],
+    ],
+];
