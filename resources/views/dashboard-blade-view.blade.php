d472032305507127218a3ca63c3fc72465e9c3d2
Daniel S. Billing <daniel@kilobyte.no>
2022-04-04 20:58:30 +0200

    Removed default dashboard and replaced with new

--- resources/views/dashboard-blade-view.blade.php
+++
@@ -1,6 +0,0 @@
-<x-dashboard>
-    <livewire:date-time-tile position="a1:a3" timezone="Europe/Oslo" />
-    <livewire:forge-server-tile position="b1:b3"/>
-    <livewire:forge-recent-events-tile position="c1:c3"/>
-    <livewire:packagist-tile position="a4:e7" />
-</x-dashboard>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,6 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+<x-dashboard>
+    <livewire:date-time-tile position="a1:a3" timezone="Europe/Oslo" />
+    <livewire:forge-server-tile position="b1:b3"/>
+    <livewire:forge-recent-events-tile position="c1:c3"/>
+    <livewire:packagist-tile position="a4:e7" />
+</x-dashboard>
