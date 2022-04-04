9d7abe1aa016b8211a5a2750a0758d63f7ec6e01
Daniel S. Billing <daniel@kilobyte.no>
2022-04-04 20:45:23 +0200

    Installed solitweb/laravel-dashboard-date-time-tile

--- resources/views/dashboard-blade-view.blade.php
+++ resources/views/dashboard-blade-view.blade.php
@@ -1,4 +1,5 @@
 <x-dashboard>
+    <livewire:date-time-tile position="a1" timezone="Europe/Oslo" />
     <livewire:forge-server-tile position="a2"/>
     <livewire:forge-recent-events-tile position="a3"/>
 </x-dashboard>
