3452bc2866fa8acc318016613fe0a93d06b3f84b
Daniel S. Billing <daniel@kilobyte.no>
2022-05-03 19:22:09 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,6 +8,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Kilobyteno\PlausibleTile\FetchDataFromPlausibleCommand;
 use TJVB\PackagistTile\FetchPackageDataCommand;
+use TJVB\PackagistTile\FetchVendorPackagesCommand;
 
 class Kernel extends ConsoleKernel
 {
@@ -30,6 +31,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command(FetchForgeServersCommand::class)->hourly();
         $schedule->command(FetchForgeRecentEventsCommand::class)->everyMinute();
+        $schedule->command(FetchVendorPackagesCommand::class)->daily();
         $schedule->command(FetchPackageDataCommand::class)->twiceDaily();
         $schedule->command(FetchDataFromPlausibleCommand::class)->hourly();
     }
