853eac0785e1bd384b5a80d44a0142a6936001cb
Daniel S. Billing <daniel@kilobyte.no>
2022-06-29 16:49:17 +0200

    Updated dashboard tile position

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-dashboard>
     <livewire:date-time-tile position="a1:a3" timezone="Europe/Oslo" />
-    <livewire:forge-server-tile position="b1:b3"/>
-    <livewire:forge-recent-events-tile position="e1:e6"/>
+    <livewire:forge-server-tile position="e1:e2"/>
+    <livewire:forge-recent-events-tile position="e3:e6"/>
     <livewire:packagist-tile position="a4:d6" />
-    <livewire:plausible-tile position="c1:d3" />
+    <livewire:plausible-tile position="b1:d3" />
 </x-dashboard>
